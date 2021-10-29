a68842770a8577a1de82eda547ba7a53bd1e882a
Johan Nordenswan <jnordenswan@gmail.com>
2021-10-29 16:45:55 +0200

    rare update

--- heavy/run.sh
+++ heavy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #stty intr ^]
-exec qemu-system-x86_64 -enable-kvm --bios /usr/share/ovmf/x64/OVMF_CODE.fd -drive format=raw,file=sl2.gpt -serial mon:stdio -net none -nographic -m 4G -smp 4
+exec qemu-system-x86_64 --bios /usr/share/ovmf/x64/OVMF_CODE.fd -drive format=raw,file=sl2.gpt -serial mon:stdio -net none -nographic -m 4G -smp 4
